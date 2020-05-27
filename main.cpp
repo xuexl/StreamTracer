4d76a56379807c824dc38f283fe5496e3bf7a83b
medo_xxl <xilin.xue@shmedo.cn>
2020-05-27 17:08:50 +0800

    1.0

--- main.cpp
+++ main.cpp
@@ -12,84 +12,81 @@
 #include<vtkPointData.h>
 #include<vtkWarpVector.h>
 #include<vtkPointSource.h>
+#include<vtkDelaunay3D.h>
+#include<vtkArrayCalculator.h>
+#include<vtkDataSetMapper.h>
+#include <vtkProperty.h>
+#include <vtkLookupTable.h>
+#include<vtkRibbonFilter.h>
 
 
+#include<QString>
+#include<string>
+#include<iostream>
 
-int main(int argc, char *argv[])
+
+namespace  {
+const char* lorentzFunc(int a, int b, double c){
+    
+    QString qs("iHat*%1*(coordsY-coordsX) + jHat*(coordsX*(%2-coordsZ)-coordsY) + kHat*(coordsX*coordsY-%3*coordsZ)");
+    std::cout<<qs.arg(a).arg(b).arg(c).toStdString().c_str()<<std::endl;
+//    return qs.arg(a).arg(b).arg(c).toStdString().data();   
+//    return "iHat*20*(coordsY-coordsX) + jHat*(coordsX*(18-coordsZ)-coordsY) + kHat*(coordsX*coordsY-2.66667*coordsZ)";
+    return "(iHat*coordsX+jHat*coordsY+kHat*coordsZ)";
+//    return "iHat";
+}
+    
+}
+
+int main()
 {
 
-    vtkSmartPointer<vtkPoints> points =
-        vtkSmartPointer<vtkPoints>::New();
-      points->InsertNextPoint(0.0, 0.0, 0.0);
-      points->InsertNextPoint(1.0, 0.0, 0.0);
-      points->InsertNextPoint(2.0, 0.0, 0.0);
-      points->InsertNextPoint(3.0, 0.0, 0.0);
-      points->InsertNextPoint(4.0, 0.0, 0.0);
+    auto points = vtkSmartPointer<vtkPointSource>::New();
+    points->SetNumberOfPoints(100);
+    points->SetDistributionToUniform();
+    points->SetCenter(0,0,0);
+    points->SetRadius(10);
     
-      vtkSmartPointer<vtkCellArray> lines =
-        vtkSmartPointer<vtkCellArray>::New();
-      vtkSmartPointer<vtkLine> line =
-        vtkSmartPointer<vtkLine>::New();
-      line->GetPointIds()->SetId(0, 0);
-      line->GetPointIds()->SetId(1, 1);
-      lines->InsertNextCell(line);
-      line->GetPointIds()->SetId(0, 1);
-      line->GetPointIds()->SetId(1, 2);
-      lines->InsertNextCell(line);
-      line->GetPointIds()->SetId(0, 2);
-      line->GetPointIds()->SetId(1, 3);
-      lines->InsertNextCell(line);
-      line->GetPointIds()->SetId(0, 3);
-      line->GetPointIds()->SetId(1, 4);
-      lines->InsertNextCell(line);
+    auto tets = vtkSmartPointer<vtkDelaunay3D>::New();
+    tets->SetInputConnection(points->GetOutputPort());
     
-      vtkSmartPointer<vtkDoubleArray> warpData =
-        vtkSmartPointer<vtkDoubleArray>::New();
-      warpData->SetNumberOfComponents(3);
-      warpData->SetName("warpData");
-      double warp[3] = {0.0, 0.0, 0.0};
-      warp[1] = 0.0;
-      warpData->InsertNextTuple(warp);
-      warp[1] = 0.1;
-      warpData->InsertNextTuple(warp);
-      warp[1] = 0.3;
-      warpData->InsertNextTuple(warp);
-      warp[1] = 0.0;
-      warpData->InsertNextTuple(warp);
-      warp[1] = 0.1;
-      warpData->InsertNextTuple(warp);
-    
-      vtkSmartPointer<vtkPolyData> polydata =
-        vtkSmartPointer<vtkPolyData>::New();
-      polydata->SetPoints(points);
-      polydata->SetLines(lines);
-      polydata->GetPointData()->AddArray(warpData);
-      polydata->GetPointData()->SetActiveVectors(warpData->GetName());
-      
-      vtkSmartPointer<vtkWarpVector> warpVector =
-        vtkSmartPointer<vtkWarpVector>::New();
-      warpVector->SetInputData(polydata);
-      warpVector->Update();
-          
+    auto calc = vtkSmartPointer<vtkArrayCalculator>::New();
+    calc->SetInputConnection(tets->GetOutputPort());    
+    calc->SetAttributeTypeToPointData();
+    calc->AddCoordinateScalarVariable("coordsX",0);
+    calc->AddCoordinateScalarVariable("coordsY",1);
+    calc->AddCoordinateScalarVariable("coordsZ",2);
+    calc->SetFunction(lorentzFunc(20,18, 8/3.0));
+    calc->SetResultArrayName("vectors");
+        
+//    double vectorsRange[2] = {0, 100};
+//    calc->GetDataSetOutput()->GetPointData()->GetArray("vectors")->GetRange(vectorsRange);
+
 
     auto smap = vtkSmartPointer<vtkPolyDataMapper>::New();
-    smap->SetInputConnection(warpVector->GetOutputPort());
+    smap->SetInputConnection(calc->GetOutputPort());
+//    smap->SelectColorArray("vectors");
+//    smap->SetScalarRange(vectorsRange);
+    
     auto sactor = vtkSmartPointer<vtkActor>::New();
     sactor->SetMapper(smap);
     
+    
+    //和图形显示有关
     auto seed = vtkSmartPointer<vtkPointSource>::New();
-    seed->SetRadius(0.1);
-    seed->SetCenter(.0,.0,.0);
-    seed->SetNumberOfPoints(8000);
+    seed->SetRadius(0.1);   //让图形更分散
+    //Center和图形原点保持一致
+    seed->SetCenter(.0,.0,.0);  //这里设置对图形的影响    ?????
+    seed->SetNumberOfPoints(10);    //每个点对应一条线
     
     auto streamers = vtkSmartPointer<vtkStreamTracer>::New();
-    streamers->SetInputConnection(warpVector->GetOutputPort());
+    streamers->SetInputConnection(calc->GetOutputPort());
     streamers->SetSourceConnection(seed->GetOutputPort());      //每个点都会计算一条流线
-    streamers->SetStartPosition(.0,.0,.0);          //设置起始点, 也可以通过指定单元id，单元子id和参数坐标来指定
+//    streamers->SetStartPosition(.0,.0,.0);          //设置起始点, 也可以通过指定单元id，单元子id和参数坐标来指定
     
     //Specify the maximum length of a streamline expressed in LENGTH_UNIT 
     //控制流线的最大长度（度量单位由MaximumPropagationUnit变量指定）
-    streamers->SetMaximumPropagation(10);  
+    streamers->SetMaximumPropagation(1000);  
     
     streamers->SetMinimumIntegrationStep(0.1);  //Specify the Minimum step size used for line integration
     streamers->SetMaximumIntegrationStep(1.0);  //Specify the Maximum step size used for line integration
@@ -98,8 +95,27 @@ int main(int argc, char *argv[])
     streamers->SetIntegrationDirectionToBoth();  //积分方向
     streamers->Update();
     
+    
+        auto magCalc = vtkSmartPointer<vtkArrayCalculator>::New();
+      magCalc->SetInputConnection(streamers->GetOutputPort());
+      magCalc->AddVectorArrayName("vectors");
+      magCalc->SetResultArrayName("MagVelocity");
+      magCalc->SetFunction("mag(vectors)");
+    
+      magCalc->Update();
+      double magVelocityRange[2];
+      magCalc->GetDataSetOutput()->GetPointData()->GetArray("MagVelocity")->GetRange(magVelocityRange);
+      
+      auto ribbonFilter = vtkSmartPointer<vtkRibbonFilter>::New();
+        ribbonFilter->SetInputConnection(magCalc->GetOutputPort());
+        ribbonFilter->SetWidth(0.005);
+    
+    
     auto mapStreamers = vtkSmartPointer<vtkPolyDataMapper>::New();
-    mapStreamers->SetInputConnection(streamers->GetOutputPort());
+    mapStreamers->SetInputConnection(magCalc->GetOutputPort());
+    mapStreamers->SelectColorArray("MagVelocity");
+     mapStreamers->SetScalarRange(magVelocityRange);
+    
 //    mapStreamers->SetScalarRange(lineSource->GetOutput()->GetPointData()->GetScalars()->GetRange());
     auto streamersActor = vtkSmartPointer<vtkActor>::New();
     streamersActor->SetMapper(mapStreamers);
